6a2aba244299dfaf726212e4c6b5a650d67b1b66
Nathan Yam <nathancyam@gmail.com>
2013-07-31 15:23:17 +1000

    Singleton design pattern added

--- DesignPatterns/Main.cs
+++ DesignPatterns/Main.cs
@@ -7,6 +7,9 @@ namespace DesignPatterns
 		public static void Main (string[] args)
 		{
 			// Abstract factory #1
+
+			Console.WriteLine ("======= Abstract Factory =======");
+
 			AbstractFactory factory1 = new ConcreteFactory1();
 			Client client1 = new Client(factory1);
 			client1.Run();
@@ -15,6 +18,8 @@ namespace DesignPatterns
 			AbstractFactory factory2 = new ConcreteFactory2();
 			Client client2 = new Client(factory2);
 			client2.Run();
+
+			Console.WriteLine ("\n======= Factory Method =======");
 
 			Document[] documents = new Document[2];
 			documents [0] = new Resume ();
@@ -25,6 +30,15 @@ namespace DesignPatterns
 				foreach(Page page in document.Pages){
 					Console.WriteLine (" " + page.GetType().Name);
 				}
+			}
+
+			Console.WriteLine ("\n======= Singleton Method =======\n");
+
+			Singleton s1 = Singleton.Instance();
+			Singleton s2 = Singleton.Instance ();
+
+			if (s1 == s2) {
+				Console.WriteLine ("Objects are the same instance");
 			}
 
 			// Wait for user input
---
+++ DesignPatterns/Singleton.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace DesignPatterns
+{
+	public class Singleton
+	{
+		private static Singleton _instance;
+
+		protected Singleton ()
+		{
+		}
+
+		public static Singleton Instance(){
+			if (_instance == null) {
+				_instance = new Singleton ();
+			}
+			return _instance;
+		}
+	}
+}
+
